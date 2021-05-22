f3cd67f1df018703ddcc85e04b49ecff62a705fc
Jeffrey Jones <jeff@jones.be>
2021-05-22 11:01:40 +0900

    Add DCT function
    Add DCT functions but leave them unimplemented. This allows DCT to
    provide its own implementation. This is a pattern we can end up using to
    provide generic'ish RPC calls and let frameworks add their own
    implementations.
    
    In thie case these functions were added in
    https://github.com/ricmzn/dct/blob/syria-at-war/src/dct/systems/overlordBotRPC.lua
    which replaces our methods as they are added later.

---
+++ lua/methods/custom.lua
@@ -0,0 +1,11 @@
+--
+-- APIs for functions that are not built-in to the DCS Mission Scripting Environment 
+--
+
+GRPC.methods.requestMissionAssignment = function(params)
+    return GRPC.errorUnimplemented("This method is not implemented")
+end
+
+GRPC.methods.joinMission = function(params)
+    return GRPC.errorUnimplemented("This method is not implemented")
+end

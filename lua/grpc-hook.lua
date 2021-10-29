3aec901a0fc9fd8dedb8587217a3225fb2292c1a
Markus Ast <m@rkusa.st>
2021-10-29 15:42:02 +0200

    catch and show errors when starting hook listeners

--- lua/grpc-hook.lua
+++ lua/grpc-hook.lua
@@ -1,38 +1,47 @@
 package.cpath = package.cpath..lfs.writedir()..[[Mods\tech\DCS-gRPC\?.dll;]]
 
+local function load()
+  log.write("[GRPC-Hook]", log.INFO, "mission loaded, setting up gRPC listener ...")
+  
+  -- Load config (written by `grpc-mission.lua`)
+  _G.GRPC = {}
+  setfenv(assert(loadfile(lfs.writedir() .. [[Data\dcs-grpc.lua]])), GRPC)()
+
+  if not GRPC.luaPath then
+    GRPC.luaPath = lfs.writedir() .. [[Scripts\DCS-gRPC\]]
+  end
+  if not GRPC.dllPath then
+    GRPC.dllPath = lfs.writedir() .. [[Mods\tech\DCS-gRPC\]]
+  end
+
+  -- Let DCS know where to find the DLLs
+  if not string.find(package.cpath, "DCS-gRPC") then
+    package.cpath = package.cpath .. GRPC.dllPath .. [[?.dll;]]
+  end
+
+  local ok, grpc = pcall(require, "dcs_grpc_hot_reload")
+  if ok then
+    log.write("[GRPC-Hook]", log.INFO, "loaded hot reload version")
+  else
+    grpc = require("dcs_grpc")
+  end
+
+  _G.grpc = grpc
+  assert(pcall(assert(loadfile(_G.GRPC.luaPath .. [[grpc.lua]]))))
+
+  log.write("[GRPC-Hook]", log.INFO, "gRPC listener set up.")
+end
+
 local function init()
   log.write("[GRPC-Hook]", log.INFO, "Initializing ...")
 
   local handler = {}
 
   function handler.onMissionLoadEnd()
-    log.write("[GRPC-Hook]", log.INFO, "mission loaded, setting up gRPC listener ...")
-
-    -- Load config (written by `grpc-mission.lua`)
-    _G.GRPC = {}
-    setfenv(assert(loadfile(lfs.writedir() .. [[Data\dcs-grpc.lua]])), GRPC)()
-
-    if not GRPC.luaPath then
-      GRPC.luaPath = lfs.writedir() .. [[Scripts\DCS-gRPC\]]
+    local ok, err = pcall(load)
+    if not ok then
+      log.write("[GRPC-Hook]", log.ERROR, "Failed to set up gRPC listener: "..tostring(err))
     end
-    if not GRPC.dllPath then
-      GRPC.dllPath = lfs.writedir() .. [[Mods\tech\DCS-gRPC\]]
-    end
-
-    -- Let DCS know where to find the DLLs
-    if not string.find(package.cpath, "DCS-gRPC") then
-      package.cpath = package.cpath .. GRPC.dllPath .. [[?.dll;]]
-    end
-
-    local ok, grpc = pcall(require, "dcs_grpc_hot_reload")
-    if ok then
-      log.write("[GRPC-Hook]", log.INFO, "loaded hot reload version")
-    else
-      grpc = require("dcs_grpc")
-    end
-
-    _G.grpc = grpc
-    assert(pcall(assert(loadfile(_G.GRPC.luaPath .. [[grpc.lua]]))))
   end
 
   function handler.onSimulationFrame()

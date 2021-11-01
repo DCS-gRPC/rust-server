0a9186520a81f8e278ac63f042ccd34beebc44ff
Jeffrey Jones <jeff@jones.be>
2021-11-01 23:22:45 +0900

    Add initial `AddGroup` implementation
    So far this implementation will create a ground group with a number of
    ground units and return null in the gRPC response for some reason.
    
    Many options are still hardcoded but what is currently implemented is
    enough to spawn a static SAM site such as a NASAM site and have it
    act as expected.

--- lua/methods/coalitions.lua
+++ lua/methods/coalitions.lua
@@ -1,10 +1,121 @@
 --
--- RPC unit actions
+-- RPC coalition actions
 -- https://wiki.hoggitworld.com/view/DCS_singleton_coalition
 --
 
 local GRPC = GRPC
 local coalition = coalition
+
+local skill = {
+  [0] = "Random",
+  [1] = "Average",
+  [2] = "Good",
+  [3] = "High",
+  [4] = "Excellent",
+  [5] = "Player",
+  Random = 0,
+  Average = 1,
+  Good = 2,
+  High = 3,
+  Excellent = 4,
+  Player = 5
+}
+
+--local altitudeType = {
+--  [1] = "BARO",
+--  [2] = "RADIO",
+--  BARO = 1,
+--  RADIO = 2
+--}
+
+local createGroundUnitsTemplate = function(unitListTemplate)
+  local units = {}
+
+  for _, unitTemplate in pairs(unitListTemplate) do
+    local pos = coord.LLtoLO(unitTemplate.position.lat, unitTemplate.position.lon)
+    local unit = {
+      name = unitTemplate.name,
+      type = unitTemplate.type,
+      x = pos.x,
+      y = pos.z,
+      transportable = { randomTransportable = false },
+      skill = skill[unitTemplate.skill],
+      heading = unitTemplate.heading,
+      playerCanDrive = true
+    }
+    table.insert(units, unit)
+  end
+
+  return units
+end
+
+local createGroundGroupTemplate = function(groupTemplate)
+  local pos = coord.LLtoLO(groupTemplate.position.lat, groupTemplate.position.lon)
+
+  local groupTable = {
+    name = groupTemplate.name,
+    route = {
+      spans = {},
+      points = {
+        {
+          x = pos.x,
+          y = pos.z,
+          type = "Turning Point",
+          eta = 0,
+          eta_locked = true,
+          alt_type = "BARO",
+          formation_template = "",
+          speed = 0,
+          action = "Off Road",
+          task = {
+            id = "ComboTask",
+            params = {
+                tasks = {}
+            }
+          }
+        }
+      }
+    },
+    task = "Ground Nothing",
+    taskSelected = true,
+    tasks = {},
+    uncontrollable = false,
+    units = createGroundUnitsTemplate(groupTemplate.units),
+    visible = false,
+    x = pos.x,
+    y = pos.z
+  }
+
+  if groupTemplate.group_id ~= nil then
+    groupTable['groupId'] = groupTemplate.group_id
+  end
+  if groupTemplate.hidden ~= nil then
+    groupTable['hidden'] = groupTemplate.hidden
+  end
+  if groupTemplate.late_activation ~= nil then
+    groupTable['lateActivation'] = groupTemplate.late_activation
+  end
+  if groupTemplate.start_time ~= nil and groupTemplate.start_time > 0 then
+    groupTable['start_time'] = groupTemplate.start_time
+  end
+  if groupTemplate.visible ~= nil then
+    groupTable['visible'] = groupTemplate.visible
+  end
+
+  return groupTable
+end
+
+GRPC.methods.addGroup = function(params)
+  if params.country_id == 0 or params.country_id == 15 then
+    return GRPC.errorInvalidArgument("invalid country code")
+  end
+
+  local template = createGroundGroupTemplate(params.template.groundTemplate)
+
+  coalition.addGroup(params.country - 1, params.groupCategory, template) -- Decrement for non zero-indexed gRPC enum
+
+  return GRPC.success({group = GRPC.exporters.group(Group.getByName(template.name))})
+end
 
 GRPC.methods.getGroups = function(params)
   local result = {}
@@ -24,7 +135,7 @@ end
 
 -- This method should be called once per coalition per mission so using COALITION_ALL to save 2
 -- API calls is not worth the extra code.
-GRPC.methods.getMainReferencePoint = function(params)
+GRPC.methods.getBullseye = function(params)
   if params.coalition == 0 then
     return GRPC.errorInvalidArgument("a specific coalition must be chosen")
   end

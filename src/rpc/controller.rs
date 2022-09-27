6cb0838f34e2229cc09ca1dca04909067c5f651c
YoloWingPixie <83574853+YoloWingPixie@users.noreply.github.com>
2022-09-26 22:20:55 -0700

    Add getDetectedTargets method to controller
    Created GRPD.methods.getDetectedTarrgets which returns position, velocity, unit name, id, and optionally descriptors.
    
    Created exporter for detectedTarget.
    
    Added GetDetectedTargets rpc call to the controller service.
    
    Added GetDetectedTargetsRequest and Response message.
    
    controller proto now imports common proto.
    
    Defined common Contact proto message

--- src/rpc/controller.rs
+++ src/rpc/controller.rs
@@ -12,4 +12,11 @@ impl ControllerService for MissionRpc {
         let res = self.request("setAlarmState", request).await?;
         Ok(Response::new(res))
     }
+    async fn get_detected_targets(
+        &self,
+        request: Request<controller::v0::GetDetectedTargetsRequest>,
+    ) -> Result<Response<controller::v0::GetDetectedTargetsResponse>, Status> {
+        let res = self.request("getDetectedTargets", request).await?;
+        Ok(Response::new(res))
+    }
 }

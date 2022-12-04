258c1ec6d75bc3f4650cbd935d290895526eed17
Markus Ast <m@rkusa.st>
2022-12-04 12:41:19 +0100

    Fix code formatting

--- src/rpc/tts.rs
+++ src/rpc/tts.rs
@@ -78,7 +78,9 @@ impl TtsService for Tts {
         let stream = client
             .start(addr, self.shutdown_signal.signal())
             .await
-            .map_err(|err| Status::internal(format!("Failed to establish SRS connection: {}", err)))?;
+            .map_err(|err| {
+                Status::internal(format!("Failed to establish SRS connection: {}", err))
+            })?;
 
         let config = match request
             .provider
